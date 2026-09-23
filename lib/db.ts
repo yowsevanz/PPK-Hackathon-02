22fcd2eceb3e9e7bde9f088987c75c801f085367
syazra <syafirazka15@gmail.com>
2026-09-23 11:47:45 +0700

    fix(auth): fixing lib/db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
+
+const connectionString = process.env.DATABASE_URL
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'], // Opsional: untuk melihat log query SQL di terminal
+    adapter,
+    log: ['query'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
